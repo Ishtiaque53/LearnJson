d9d298d82ab4a8e73dcaf92990039a0a90d89a54
Ishtiaque53 <ishtiaquerabbi@gmail.com>
2021-03-15 13:51:47 +0600

    commit

---
+++ src/com/LearningJSON/StudentInput.java
@@ -0,0 +1,17 @@
+package com.LearningJSON;
+
+
+import java.util.Scanner;
+
+public class StudentInput {
+    private int studentIdInput;
+
+    Scanner scanner = new Scanner(System.in);
+
+    public int getStudentId(){
+        System.out.print("Student ID: ");
+        studentIdInput = scanner.nextInt();
+        return studentIdInput;
+    }
+
+}
